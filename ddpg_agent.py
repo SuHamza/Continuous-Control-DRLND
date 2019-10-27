222a59803d738c3e00d42d54b399812eda9c76bf
Suzan Hamza <suzan.hamza@gmail.com>
2019-10-27 20:56:49 +0200

    Update max_t & seed
    max_t = 1000
    seed = 1
    Better learning but didn't converge yet!

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -17,12 +17,12 @@ BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_ACTOR = 1e-3         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
 LEARN_EVERY = 20        # Learning timestep interval
-LEARN_NUM = 10          # Number of learning passes
+LEARN_NUM = 1           # Number of learning passes
 GRAD_CLIPPING = 1.0     # Gradient Clipping
 
 # Noise Parameters
